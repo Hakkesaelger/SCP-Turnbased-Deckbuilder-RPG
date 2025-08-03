c8794c6aaaa72c1afb2e4084e3907308c1a23e85
Vilhelm <vilhelmroennenielsen@gmail.com>
2025-08-03 12:24:07 +0200

    CardClass

---
+++ script.js
@@ -0,0 +1,10 @@
+let humesPlayer=10;
+let humesOpponent=10;
+class Card{
+    constructor(card){
+        this.health=card["health"]
+        this.mainAbility=card["mainAbility"]
+        this.passiveAbility=card["passiveAbility"]
+        this.specialAbility=card["specialAbility"]
+    }
+}
