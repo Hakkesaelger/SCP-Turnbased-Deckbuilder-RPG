182a0273c31c93008d2e8c9418442085441a8bfa
Vilhelm <vilhelmroennenielsen@gmail.com>
2025-08-03 13:18:05 +0200

    Update

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let humesPlayer=10;
-let humesOpponent=10;
+const player={humes:10,hand:[]};
+const opponent={humes:10,hand:[]};
 class Card{
-    constructor(card){
-        this.health=card["health"]
-        this.mainAbility=card["mainAbility"]
-        this.passiveAbility=card["passiveAbility"]
-        this.specialAbility=card["specialAbility"]
+    constructor(card, player){
+        this.image=card.image
+        this.name=card.name
+        this.health=card.health;
+        this.mainAbility=card.mainAbility;
+        this.passiveAbility=card.passiveAbility;
+        this.specialAbility=card.specialAbility;
+        this.trigger=card.trigger
+        this.player=player;
     }
+    takeDamage(damage){
+        this.health-=damage
+        if (this.health<=0){
+            const index=this.player.hand.indexOf(this);
+            this.player.hand.splice(index,1)
+        }
+    }
+    useMainAbility(target,used){this.mainAbility(target,used);}
+    checkPassive(){if (this.trigger&&this.passiveAbility){this.passiveAbility()}}
+    useSpecialAbility(target){if (this.specialAbility){this.specialAbility(target)};}
 }
